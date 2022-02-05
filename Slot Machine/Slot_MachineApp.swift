009904ac9461f112a6c2e217f11dd3e474271bcf
Justice2000 <91248448+Justice2000@users.noreply.github.com>
2022-02-05 00:49:43 -0500

    Initial Commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Justice Ekeke on 05/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
